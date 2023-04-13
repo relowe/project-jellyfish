1908f06b000a671bf6c4f9a728804d901800d7de
Nathan Kiehl <61288263+Kiehlster64@users.noreply.github.com>
2023-04-13 15:29:43 -0500

    Redo Symbol Table
    It's better now. And it doesn't actually hold values. These are simpler times.

--- code/src/bin/sa2.rs
+++ code/src/bin/sa2.rs
@@ -3,8 +3,6 @@
 use std::{env, process};
 use std::collections::HashMap;
 use crate::parser;
-use crate::lexer::{Token, TokenType};
-use crate::parser::{ParseTree, ParseType};
 
 use std::collections::HashMap;
 
@@ -171,92 +169,8 @@ impl SymbolTable {
     }
 }
 
-<<<<<<< Updated upstream
-// Define a struct called SemanticAnalyzer that has a HashMap called symbol_table, which maps strings to Symbols
-pub struct SemanticAnalyzer {
-    symbol_table: HashMap<String, Symbol>,
-}
-// Implement a constructor function for the struct that returns a new instance of SemanticAnalyzer
-impl SemanticAnalyzer {
-    pub fn new() -> Self {
-        SemanticAnalyzer {
-            symbol_table: HashMap::new(),
-        }
-    }
-    
-    // Implement a method called analyze that takes a ParseTree as input and returns a Result type with no value if successful or a String if an error occurs
-    pub fn analyze(&mut self, parse_tree: ParseTree) -> Result<(), String> {
-        match parse_tree.parse_type {
-            ParseType::CODE => {
-                for statement in parse_tree.children {
-                    if statement.is_some() {
-                        self.analyze_statement(statement.unwrap())?;
-                    }
-                }
-            }
-            _ => return Err("Invalid parse tree".to_string()),
-        }
-        Ok(())
-    }
-
-    // Define a method called analyze_statement that takes a ParseTree as input and returns a Result type with no value if successful or a String if an error occurs
-    fn analyze_statement(&mut self, statement: ParseTree) -> Result<(), String> {
-        match statement {
-            ParseTree::parse_type::Assignment(identifier, expression) => {
-                let symbol = self.get_symbol(&identifier)?;
-                let expression_type = self.analyze_expression(expression)?;
-                if symbol.data != expression_type {
-                    return Err(format!(
-                        "Type mismatch: expected {:?}, found {:?}",
-                        symbol.data, expression_type
-                    ));
-                }
-            }
-            _ => return Err("Invalid statement".to_string()),
-        }
-        Ok(())
-    }
-
-    // Define a method called analyze_expression that takes a ParseTree as input and returns a Result type with a SymbolData if successful or a String if an error occurs
-    // TODO add support for more types, figure out issue with abiguous associated types?
-    fn analyze_expression(&mut self, expression: ParseTree) -> Result<SymbolData, String> {
-        match expression {
-            ParseTree::parse_type::BINOP(op, left, right) => {
-                let left_type = self.analyze_expression(*left)?;
-                let right_type = self.analyze_expression(*right)?;
-                if left_type != right_type {
-                    return Err(format!(
-                        "Type mismatch: expected {:?}, found {:?}",
-                        left_type, right_type
-                    ));
-                }
-                match op {
-                    TokenType::ADD => Ok(left_type),
-                    _ => Err("Invalid operator".to_string()),
-                }
-            }
-            ParseTree::parse_type::NUMBER_TYPE(value) => Ok(SymbolData::NUMBER(value)),
-            ParseTree::parse_type::ID(identifier) => {
-                let symbol = self.get_symbol(&identifier)?;
-                Ok(symbol.data.clone())
-            }
-            _ => Err("Invalid expression".to_string()),
-        }
-    }
-    
-
-    fn get_symbol(&self, identifier: &str) -> Result<&Symbol, String> {
-        match self.symbol_table.get(identifier) {
-            Some(symbol) => Ok(symbol),
-            None => Err(format!("Undefined symbol: {}", identifier)),
-        }
-    }
-}
-
-=======
 fn main() {
     let mut st = SymbolTable::new();
->>>>>>> Stashed changes
 
     st.add_type("PERSON".to_string());
 
